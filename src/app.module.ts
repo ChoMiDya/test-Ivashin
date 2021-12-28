c305ec208b67e42f6dfc8224561d17ae88e4044f
ChoMiDya <vikcha26101999@gmail.com>
2021-12-28 23:56:02 +1000

    set postgres typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfig } from './configs/typeorm.config';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ UserModule ],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRootAsync({useClass: TypeOrmConfig}),
+  ],
 })
 export class AppModule {}
---
+++ src/config.ts
@@ -0,0 +1,7 @@
+import { config } from 'dotenv';
+import * as path from 'path';
+
+config({
+  path: path.join(__dirname, "../env", `${process.env.NODE_ENV}.env`)
+});
+
